dca2939365ffad51307cfd2439bb7e0b6022d78b
Frotrest <125584696+frotrest@users.noreply.github.com>
2026-01-11 15:15:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
@@ -38,8 +36,6 @@ export default defineConfig(({ command }) => {
       emptyOutDir: true,
     },
     plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
       SortCss({
         sort: 'mobile-first',
       }),
