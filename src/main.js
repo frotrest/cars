717ac33abf3df5e073af1791e6a703d368c957cd
Frotrest <125584696+frotrest@users.noreply.github.com>
2026-01-12 19:31:02 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -1,34 +1,34 @@
 /* Active Nav Link on Scrol */
 
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
   let top = window.scrollY; // Поточне значення прокрутки
 
   if (top === 0) {
     // Якщо прокрутка на верхній частині
-    navLinks.forEach((link) => {
-      link.classList.remove("active"); // Видаляємо активний клас у всіх посиланнях
+    navLinks.forEach(link => {
+      link.classList.remove('active'); // Видаляємо активний клас у всіх посиланнях
     });
 
     // Додаємо активний клас до першого посилання
-    navLinks[0].classList.add("active");
+    navLinks[0].classList.add('active');
   } else {
-    sections.forEach((sec) => {
+    sections.forEach(sec => {
       let offset = sec.offsetTop - 150; // Верх секції
       let height = sec.offsetHeight; // Висота секції
-      let id = sec.getAttribute("id"); // ID секції
+      let id = sec.getAttribute('id'); // ID секції
 
       // Перевіряємо, чи прокрутка в межах секції
       if (top >= offset && top < offset + height) {
-        navLinks.forEach((link) => {
-          link.classList.remove("active"); // Видаляємо активний клас у всіх посиланнях
+        navLinks.forEach(link => {
+          link.classList.remove('active'); // Видаляємо активний клас у всіх посиланнях
 
           // Вибираємо посилання відповідно до ID секції
           document
             .querySelector("header nav a[href*='" + id + "']")
-            .classList.add("active"); // Додаємо активний клас
+            .classList.add('active'); // Додаємо активний клас
         });
       }
     });
@@ -37,24 +37,24 @@ window.onscroll = () => {
 
 /* Active block menu & logo & languages on Scrol */
 
-let menuBlock = document.querySelector(".header__blocks");
+let menuBlock = document.querySelector('.header__blocks');
 
 let toggleMenuOnScroll = () => {
   let pos2 = document.documentElement.scrollTop;
 
   if (pos2 > 50) {
     // Прокручено більше ніж на 100px
-    menuBlock.classList.add("active");
-    menuBlock.classList.add("open");
+    menuBlock.classList.add('active');
+    menuBlock.classList.add('open');
   } else {
-    menuBlock.classList.remove("active");
-    menuBlock.classList.remove("open");
+    menuBlock.classList.remove('active');
+    menuBlock.classList.remove('open');
   }
 };
 
 let calcScrollValue = () => {
-  let scrollProgress = document.getElementById("progress");
-  let progressValue = document.getElementById("progress__value");
+  let scrollProgress = document.getElementById('progress');
+  let progressValue = document.getElementById('progress__value');
   let pos = document.documentElement.scrollTop;
 
   let calcHeight =
@@ -64,12 +64,12 @@ let calcScrollValue = () => {
   let scrollValue = Math.round((pos * 100) / calcHeight);
 
   if (pos > 100) {
-    scrollProgress.style.display = "grid";
+    scrollProgress.style.display = 'grid';
   } else {
-    scrollProgress.style.display = "none";
+    scrollProgress.style.display = 'none';
   }
 
-  scrollProgress.addEventListener("click", () => {
+  scrollProgress.addEventListener('click', () => {
     document.documentElement.scrollTop = 0;
   });
 
@@ -77,8 +77,8 @@ let calcScrollValue = () => {
 };
 
 // Додаємо обидві функції для події onscroll
-window.addEventListener("scroll", toggleMenuOnScroll);
-window.addEventListener("scroll", calcScrollValue);
+window.addEventListener('scroll', toggleMenuOnScroll);
+window.addEventListener('scroll', calcScrollValue);
 
 // Викликаємо функцію при завантаженні сторінки
 window.onload = calcScrollValue;
